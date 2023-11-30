69ec7416a80bb302142b25c70fa83e2dabf69f2f
Cassandre <cassandre.chandelier@ensea.fr>
2023-11-30 11:18:44 +0100

    Question 2

--- README.md
+++ README.md
@@ -1,7 +1,18 @@
-### enseash
+# enseash
 
 
 ## Question 1
 
 
 The message of welcome appears without any problem
+
+## Question 2
+
+We have an input for the user, it is executed. After the execution, we print
+the prompt enseash % and wait for new command. 
+We created a fonction prompt to print each time 'enseash %'
+
+```bash
+# define PROMPT "enseash % "
+write(1,PROMPT,sizeof(PROMPT));
+```
--- main.c
+++ main.c
@@ -2,16 +2,53 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 #define WELCOME "Welcome to ENSEA Tiny Shell\r\nType 'exit' to quit\r\nenseash % "
+#define PROMPT "enseash % "
 
-void welcomeMessage(){
-        write(1, WELCOME, sizeof(WELCOME)) ;
+#define NBYTES 128
+
+void welcomeMessage(){ write(1, WELCOME, sizeof(WELCOME));}
+
+void prompt(){ write(1,PROMPT,sizeof(PROMPT));}
+
+
+void executionCommand() {
+
+	while(1) {
+    		char command[NBYTES];
+
+    		//input
+		ssize_t input = read(1, command, sizeof(command));
+
+    		if (input == -1) { perror("read"); }
+
+    		//replace the last character '\n'
+    		if (input > 0 && command[input-1] == '\n') { command[input-1] = '\0';}
+
+    		pid_t childPid = fork();
+
+    		if (childPid == -1) { perror("fork");} //error
+
+    		if (childPid == 0) { //son
+        		execlp(command, command, (char *)NULL);
+        		perror("execlp");
+        		exit(1);
+    		} else { //parent
+        		waitpid(childPid, NULL, 0);
+    		}
+		prompt();
+
+	}
+
+
 }
 
 int main(void){
 
         welcomeMessage() ;
+	executionCommand();
 
         return EXIT_SUCCESS ;
 }
