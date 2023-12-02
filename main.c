1890692ba241d75207e6f1e5e74f989a0de48d66
Cassandre <cassandre.chandelier@ensea.fr>
2023-12-02 13:22:57 +0100

    Question 3

--- main.c
+++ main.c
@@ -4,53 +4,58 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-#define WELCOME "Welcome to ENSEA Tiny Shell\r\nType 'exit' to quit\r\nenseash % "
 #define PROMPT "enseash % "
+#define WELCOME "Welcome to ENSEA Tiny Shell\r\nType 'exit' to quit\r\n"PROMPT
+#define EXITMESSAGE "Bye bye...\r\n"
 
 #define NBYTES 128
 
-void welcomeMessage(){ write(1, WELCOME, sizeof(WELCOME));}
+void prompt(char *message){ write(1,message,strlen(message));}
 
-void prompt(){ write(1,PROMPT,sizeof(PROMPT));}
+void welcomeMessage(){prompt(WELCOME);}
 
+void executionCommand(char *command) {
 
-void executionCommand() {
+	pid_t childPid = fork();
 
-	while(1) {
-    		char command[NBYTES];
+	if (childPid == -1) { perror("fork");} //error
 
-    		//input
-		ssize_t input = read(1, command, sizeof(command));
+	if (childPid == 0) { //son
+		execlp(command, command, (char *)NULL);
+		perror("execlp");
+		exit(1);
 
-    		if (input == -1) { perror("read"); }
-
-    		//replace the last character '\n'
-    		if (input > 0 && command[input-1] == '\n') { command[input-1] = '\0';}
-
-    		pid_t childPid = fork();
-
-    		if (childPid == -1) { perror("fork");} //error
-
-    		if (childPid == 0) { //son
-        		execlp(command, command, (char *)NULL);
-        		perror("execlp");
-        		exit(1);
-    		} else { //parent
-        		waitpid(childPid, NULL, 0);
-    		}
-		prompt();
-
+	} else { //parent
+      		waitpid(childPid, NULL, 0);
 	}
-
-
+	prompt(PROMPT);
 }
+
 
 int main(void){
 
-        welcomeMessage() ;
-	executionCommand();
+	char command[NBYTES];
+	ssize_t input;
 
-        return EXIT_SUCCESS ;
+       	welcomeMessage() ;
+
+	while(1){
+
+		input = read(STDIN_FILENO,command, sizeof(command));
+
+		if (input == -1 || input == 0) {
+			write(STDERR_FILENO, EXITMESSAGE, sizeof(EXITMESSAGE));
+			exit(EXIT_FAILURE);
+		}
+
+		command[input-1]='\0';
+
+		if (strcmp(command,"exit")==0){
+			write(STDOUT_FILENO,EXITMESSAGE,sizeof(EXITMESSAGE));
+			exit(EXIT_SUCCESS);
+		}
+		executionCommand(command);
+	}
+	return EXIT_SUCCESS;
 }
-
 
