d3942c7432a723be7b93e3393eb8081f240df55b
Cassandre <cassandre.chandelier@ensea.fr>
2023-12-04 11:47:19 +0100

    Question 5

--- README.md
+++ README.md
@@ -35,3 +35,27 @@ false if it didn't). Then, we use WEXITSTATUS which returns the exit code only i
 WIFEXITED returns true. For returning the signal, we do the same with two others 
 functions. WIFSIGNALED returns true if the son end because of a signal and WTERMSIG 
 returns the number of the signal that caused the end of the son.
+
+
+## Question 5
+
+We want to add the time in the function return. So by using the function clock_gettime, we 
+can use the variable CLOCK_REALTIME which is the time at the moment.
+To use this function, we had to define a structure:
+
+```bash
+struct timespec start, end;
+```
+
+We use at the beginning of the function executionCommand by calling the structure (&start),
+and at the end of the execution (in the parent process) by calling the structure a last 
+time (&end). Then, we calculate the time elapsed.
+
+Moreover, we can see that we have the same code two times, so we create another function: 
+
+```bash
+wifReturn(int status, const char *type, int value, struct timespec *start, struct 
+timespec *end);
+```
+
+Then it can return WIFEXITED or WIFSIGNAL.
--- main.c
+++ main.c
@@ -4,26 +4,48 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <time.h>
 
 #define ENSEASH "enseash "
 #define PROMPT ENSEASH"% "
 #define WELCOME "Welcome to ENSEA Tiny Shell\r\nType 'exit' to quit\r\n"PROMPT
 #define EXITMESSAGE "Bye bye...\r\n"
 
+
 #define NBYTES 128
+#define MILLION 1000000.0
 
 void prompt(char *message){ write(1,message,strlen(message));}
 
 void welcomeMessage(){prompt(WELCOME);}
 
+void wifReturn(int status, const char *type, int value, struct timespec *start, struct timespec *end){
+
+	clock_gettime(CLOCK_REALTIME,end);
+	double time_spent = (end->tv_sec - start->tv_sec)*1000.0 +
+		(end->tv_nsec - start->tv_nsec)/MILLION;
+
+	char statusMessage[NBYTES];
+	prompt(ENSEASH);
+	sprintf(statusMessage,"[%s: %d|%0.0fms]",type,value,time_spent);
+	write(STDOUT_FILENO,statusMessage,strlen(statusMessage));
+	prompt(" % ");
+}
+
+
 void executionCommand(char *command) {
+
+	struct timespec start, end;
+	clock_gettime(CLOCK_REALTIME,&start);
 
 	pid_t childPid = fork();
 
 	if (childPid == -1) { perror("fork");} //error
 
 	if (childPid == 0) { //son
+
 		execlp(command, command, (char *)NULL);
+
 		perror("execlp");
 		exit(1);
 
@@ -32,21 +54,12 @@ void executionCommand(char *command) {
       		waitpid(childPid, &status, 0);
 
 		if (WIFEXITED(status)){ //return code
-			char statusMessage[NBYTES];
-			prompt(ENSEASH);
-			sprintf(statusMessage,"[exit: %d]",WEXITSTATUS(status));
-			write(STDOUT_FILENO,statusMessage,strlen(statusMessage));
-			prompt(" % ");
-		}
+
+			wifReturn(status,"exit",WEXITSTATUS(status),&start,&end);}
 
 		else if (WIFSIGNALED(status)){//return signal if end of son because of signal
-			char statusMessage[NBYTES];
-			prompt(ENSEASH);
-			sprintf(statusMessage,"[sig: %d]",WTERMSIG(status));
-			write(STDOUT_FILENO,statusMessage,strlen(statusMessage));
-			prompt(" % ");
 
-		}
+			wifReturn(status,"sign",WTERMSIG(status),&start,&end);}
 	}
 }
 
@@ -73,6 +86,7 @@ int main(void){
 			write(STDOUT_FILENO,EXITMESSAGE,sizeof(EXITMESSAGE));
 			exit(EXIT_SUCCESS);
 		}
+
 		executionCommand(command);
 	}
 	return EXIT_SUCCESS;
